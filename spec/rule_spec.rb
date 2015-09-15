02beeb8131b592f3e6b3130ee02acecfc4c2b95d
brian m. carlson <sandals@crustytoothpaste.net>
2015-09-15 01:07:02 +0000

    Add spec for matching rules.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/rule_spec.rb
+++ spec/rule_spec.rb
@@ -33,4 +33,18 @@ EOM
     parser = Picobot::RuleParser.new
     expect { parser.parse(rules) }.to raise_exception Picobot::RuleConflictError
   end
+
+  it 'should match rules correctly' do
+    rules = <<EOM
+0 *x** -> E 0
+EOM
+    parser = Picobot::RuleParser.new
+    res = nil
+    expect { res = parser.parse(rules) }.not_to raise_exception
+    r = res[0]
+    expect(r.match?(0, true, false, true, true)).to be true
+    expect(r.match?(0, false, false, true, true)).to be true
+    expect(r.match?(0, true, true, true, true)).to be false
+    expect(r.match?(2, true, false, true, true)).to be false
+  end
 end
